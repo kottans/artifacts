17bb1703dfd032fab3d73738be05d2434095ce53
Mykola <feanorfeanturi@gmail.com>
2016-07-26 02:05:01 +0300

    moar spaces

--- wear_sources/js-course-shirt/js2016-3/jsCourse.js
+++ wear_sources/js-course-shirt/js2016-3/jsCourse.js
@@ -5,19 +5,19 @@ let jsCourse = {
     session : 'JS2016-3',
     location : {
         country :'Ukraine',
-        cities : ['Kyiv','Kharkiv','Lviv']    
+        cities : ['Kyiv', 'Kharkiv', 'Lviv']    
     },
     dates : {
         start : new Date ('May 12, 2016'),
         end : new Date ('July 30, 2016')
     },
     program : {
-        basic : ['(Post)?HTML','(Post)?CSS','Semantics','ES6'],
-        server : ['NodeJS','ember-cli'],
-        testing : ['TDD','Mocha','Chai'],
+        basic : ['(Post)?HTML', '(Post)?CSS', 'Semantics','ES6'],
+        server : ['NodeJS', 'ember-cli'],
+        testing : ['TDD', 'Mocha', 'Chai'],
         concepts : ['REST'],
-        ci_cd : ['Webpack','Travis'],
-        frameworks : ['React','Angular','Polymer','Ember'],
+        ci_cd : ['Webpack', 'Travis'],
+        frameworks : ['React', 'Angular', 'Polymer', 'Ember'],
     }
 }
 Kottan.use(jsCourse).then(() => { console.log('Very promising!') })
